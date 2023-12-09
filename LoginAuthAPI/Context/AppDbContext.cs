22c283da571238dd30bea254a5252f222cd84d34
Rafael <rafael.miranda079@gmail.com>
2023-12-08 23:15:14 -0300

    adding database

---
+++ LoginAuthAPI/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using LoginAuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace LoginAuthAPI.Context
+{
+    public class AppDbContext: DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        { 
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+
+    }
+}
--- LoginAuthAPI/obj/Debug/net8.0/LoginAuthAPI.AssemblyInfo.cs
+++ LoginAuthAPI/obj/Debug/net8.0/LoginAuthAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginAuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08547d28eeeedd1101f1ab086b1f0f52b38297ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginAuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginAuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
